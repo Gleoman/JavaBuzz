3b8c98c4ebb940a928a98b58f5f50a7ab4f0ded4
Ed Goold <teddy_goold@hotmail.com>
2018-01-29 17:14:24 +0000

    final 1st day

--- jasmine/src/javaBuzz.js
+++ jasmine/src/javaBuzz.js
@@ -4,6 +4,7 @@ function Javabuzz() {
 }
 
 Javabuzz.prototype.says = function(first_argument) {
+  console.log("this is running")
   if (first_argument % 3 == 0 && first_argument % 5 == 0 )
     { return "JavaBuzz" }
   else if (first_argument % 3 == 0)
@@ -13,3 +14,13 @@ Javabuzz.prototype.says = function(first_argument) {
   else
     { return first_argument }
 };
+
+Javabuzz.prototype.output = function(first_argument) {
+  for (var i = 1; i <= 100; i++) {
+  console.log(this.says(i));
+  }
+};
+
+var javaBuzz = new Javabuzz();
+
+javaBuzz.output
